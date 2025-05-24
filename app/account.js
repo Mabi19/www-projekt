971dd042c9719487c523c7191f1c8e7d7877d3f9
Mabi19 <mateuszbartnik33@gmail.com>
2025-05-24 13:36:50 +0200

    Fix login form

---
+++ app/account.js
@@ -0,0 +1,7 @@
+const username = document.cookie.split("; ").find((entry) => entry.startsWith("user=")).split("=").at(-1);
+
+document.querySelector("#account-username").textContent = username;
+document.querySelector("#logout-button").addEventListener("click", () => {
+    document.cookie = "user=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.href = "/"
+})
