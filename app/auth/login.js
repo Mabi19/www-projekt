124b8d5c3c292e1186752fee77402bdc371659e2
Mabi19 <mateuszbartnik33@gmail.com>
2025-06-08 21:19:48 +0200

    Add better login form validation

--- app/auth/login.js
+++ app/auth/login.js
@@ -1,27 +1,41 @@
 const loginForm = document.querySelector("#login");
 const usernameInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
+const resultBox = document.querySelector("#result");
+
+function setResult(str) {
+    if (str) {
+        resultBox.textContent = str;
+        resultBox.classList.remove("hidden");
+    } else {
+        resultBox.classList.add("hidden");
+    }
+}
+
+usernameInput.addEventListener("input", () => resultBox.classList.add("hidden"));
+passwordInput.addEventListener("input", () => resultBox.classList.add("hidden"));
 
 loginForm.addEventListener("submit", async (ev) => {
     ev.preventDefault();
     const username = usernameInput.value;
     const password = passwordInput.value;
+    if (!username) {
+        setResult("Nazwa użytkownika jest wymagana");
+        return;
+    }
+
+    if (!password) {
+        setResult("Hasło jest wymagane");
+    }
 
     const res = await fetch("/login", { method: "POST", body: JSON.stringify({ username, password }) });
     if (res.ok) {
         window.location.href = "./browse.html";
     } else {
-        let resultBox = loginForm.querySelector("#result");
-        if (!resultBox) {
-            resultBox = document.createElement("div");
-            resultBox.id = "result";
-            loginForm.appendChild(resultBox);
-        }
-
         if (res.status == 401) {
-            resultBox.textContent = "Niepoprawny login lub hasło";
+            setResult("Niepoprawny login lub hasło");
         } else {
-            resultBox.textContent = "Nieznany błąd :(";
+            setResult("Nieznany błąd :(");
         }
     }
 });
